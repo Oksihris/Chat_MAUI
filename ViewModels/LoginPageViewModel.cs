e3815320f0199758d2b84511293c2a4fd69d6782
Oksihris <112577436+Oksihris@users.noreply.github.com>
2023-04-27 17:45:36 +0300

    Login logic

--- ViewModels/LoginPageViewModel.cs
+++ ViewModels/LoginPageViewModel.cs
@@ -1,12 +1,96 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ChatAppSR.ViewModels
 {
-    public class LoginPageViewModel
+    public class LoginPageViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));      
+
+        }
+
+        public LoginPageViewModel()
+        {
+            UserName = "";
+            Password = "";
+            isProcessing = false;
+
+            LoginCommand = new Command(() =>
+            {
+                if (IsProcessing) return;
+                if (UserName.Trim() == "" ||  Password.Trim() == "") return;
+
+                isProcessing = true;
+                Login().GetAwaiter().OnCompleted(() =>
+                {
+                    isProcessing = false;
+                });
+
+            });
+        }     
+
+        async Task Login()
+        {
+            try { 
+            var request = new AuthenticateRequest
+            {
+                LoginId = UserName,
+                Password = Password,
+            };
+
+            var response = await ServiceProvider.GetInstance().Authenticate(request);
+            if (response.StatusCode == 200) 
+            {
+                    await AppShell.Current.DisplayAlert("ChatApp",
+                    "Login sucessful! \n" +
+                    $"UserName: {response.UserName}\n" +
+                    $"Token: {response.Token}", "OK");
+              }
+            else
+            {
+                    await AppShell.Current.DisplayAlert("ChatApp", response.StatusMessage, "OK");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                await AppShell.Current.DisplayAlert("ChatApp", ex.Message, "OK");
+
+
+            }
+
+        }
+
+        private string userName;
+        private string password;
+        private bool isProcessing;
+
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; OnPropertyChanged(); }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; OnPropertyChanged(); }
+        }
+
+        public bool IsProcessing
+        {
+            get { return isProcessing; }
+            set { isProcessing = value; OnPropertyChanged();}   
+        }
+        public ICommand LoginCommand { get; set; }
     }
 }
